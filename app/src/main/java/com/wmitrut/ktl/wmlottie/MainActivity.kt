939fefd49ee7a57fdae617b5ab22816728129955
wmitrut <wellingtonmitrut@gmail.com>
2018-02-13 17:41:23 -0200

    Code improvements and added stop animation method

--- app/src/main/java/com/wmitrut/ktl/wmlottie/MainActivity.kt
+++ app/src/main/java/com/wmitrut/ktl/wmlottie/MainActivity.kt
@@ -2,6 +2,7 @@ package com.wmitrut.ktl.wmlottie
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.view.View
 import com.airbnb.lottie.LottieAnimationView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,18 +14,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val animationView = findViewById<View>(R.id.animation_view) as LottieAnimationView;
-        val pauseButton = findViewById<View>(R.id.button_pause);
+        val animationView = findViewById<View>(R.id.animation_view) as LottieAnimationView
+        val pauseButton = findViewById<View>(R.id.button_pause)
+        val stopButton = findViewById<View>(R.id.button_stop)
 
-        pauseButton.setOnClickListener(){
+        pauseButton.setOnClickListener{
             if (animationView.isAnimating()) {
-                animationView.pauseAnimation();
+                animationView.pauseAnimation()
                 button_pause.setImageResource(R.drawable.play)
             }else {
-                animationView.resumeAnimation();
+                animationView.resumeAnimation()
                 button_pause.setImageResource(R.drawable.pause)
             }
         }
+
+        stopButton.setOnClickListener{
+            animationView.cancelAnimation()
+            animationView.setProgress(0F)
+            button_pause.setImageResource(R.drawable.play)
+        }
+
 
     }
 }
